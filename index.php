97446ec59c1977715e5dcc2974449cf0e9d7173e
karina BRANTE <karina.brante@gmail.com>
2017-05-23 15:19:43 +0200

    add loop of posts

--- index.php
+++ index.php
@@ -3,10 +3,26 @@
 <body>
     <div class="row">
         <div class="col-md-12">
+            <?php
 
+            $cats = get_categories();
+            foreach ($cats as $cat) {
+                $args = array('category' => $cat->cat_ID);
+                $posts = get_posts($args);
+                foreach ($posts as $post) {
+                    setup_postdata($post);
+                    the_title('<h3>', '</h3>');
+                    the_excerpt();
+                    the_meta();
+                    $mykey_values = get_post_custom_values('Price');
+                    foreach ($mykey_values as $value) {
+                    }
+                }
+            }
+            ?>
         </div>
     </div>
 
 
 
-<?php get_footer();?>
+    <?php get_footer();?>
